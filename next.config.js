2262cb61c663627ff987d83fe44a9764a2a63cb2
Ryzen <ryzen0371@gmail.com>
2026-02-01 05:41:37 +0000

    Build: Use standalone output for dynamic deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable static optimization for dynamic Q&A platform
-  staticPageGenerationTimeout: undefined,
+  output: 'standalone',
   swcMinify: true,
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
